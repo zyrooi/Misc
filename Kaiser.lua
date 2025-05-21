e6dfca9641cd26dad03481009ab4d0bba0328de2
zyrooi <kietblox7777@gmail.com>
2025-05-21 20:48:37 +0700

    Update Kaiser.lua

--- Kaiser.lua
+++ Kaiser.lua
@@ -1,5 +1,5 @@
-if game.GameId == 5430301679 then
-elseif game.GameId == 1234567890 then
+if game.GameId == 7074860883 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/zyrooi/months/refs/heads/main/Premium%20(1).txt"))()
 else
   game.Players.LocalPlayer:Kick("[ Not Support ]")
 end
